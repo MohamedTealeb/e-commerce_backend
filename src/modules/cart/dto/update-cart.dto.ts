aeb0fbb4b81296a78028c1ef2472da32d60cb837
MohamedTealeb <mohamedtealeb088@gmail.com>
2025-12-02 10:15:03 +0200

    remove from cart

--- src/modules/cart/dto/update-cart.dto.ts
+++ src/modules/cart/dto/update-cart.dto.ts
@@ -2,14 +2,24 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateCartDto } from './create-cart.dto';
 import { Types } from 'mongoose';
 import { MongoDBIds } from 'src/common/decoretors/match.custom.decoretor';
-import { Validate } from 'class-validator';
+import { Validate, IsArray, ValidateNested, IsOptional, IsNumber, Min, IsPositive, IsMongoId } from 'class-validator';
+import { Type } from 'class-transformer';
 
+export class RemoveCartItem {
+    @IsMongoId()
+    productId: Types.ObjectId;
+    
+    @IsOptional()
+    @IsNumber()
+    @Min(1)
+    @IsPositive()
+    quantity?: number;
+}
 
 export class RemoveItemCartDto  {
-
-
-
-    @Validate(MongoDBIds)
-    productId:Types.ObjectId[]
+    @IsArray()
+    @ValidateNested({ each: true })
+    @Type(() => RemoveCartItem)
+    items: RemoveCartItem[];
 }
 export class UpdateCartDto extends PartialType(CreateCartDto) {}
